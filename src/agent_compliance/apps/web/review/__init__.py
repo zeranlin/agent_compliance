efbc716725d25a05190635ece990b7fa561ee379
林泽然 <zeranlin@163.com>
2026-03-20 14:20:20 +0800

    让review-check和review-next页面开始脱离web入口文件

---
+++ src/agent_compliance/apps/web/review/__init__.py
@@ -0,0 +1,4 @@
+from agent_compliance.apps.web.review.buyer_page import review_buyer_html
+from agent_compliance.apps.web.review.next_page import review_next_html
+
+__all__ = ['review_buyer_html', 'review_next_html']
